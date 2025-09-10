2b4629f1f7c8c89e0d01bfcf28253cf4cd0c7f06
Fabien Sanchez <fabien.sanchez@citopia.fr>
2025-09-10 15:33:32 +0200

    feat: enhance BlueskyController with logging for client metadata and connect URL endpoints

--- packages/api/src/bluesky/bluesky.controller.ts
+++ packages/api/src/bluesky/bluesky.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Header } from "@nestjs/common";
+import { Controller, Get, Header, Logger, Query } from "@nestjs/common";
 import { BlueskyService } from "./bluesky.service";
 import { ClientMetadataEntity, ConnectUrlResponseEntity } from "./entities";
 
 @Controller("bluesky")
 export class BlueskyController {
+  private logger = new Logger(BlueskyController.name);
   constructor(private readonly blueskyService: BlueskyService) {}
 
   @Get("client-metadata.json")
   @Header("Content-Type", "application/json")
   @Header("Access-Control-Allow-Origin", "*")
   getClientMetadata(): ClientMetadataEntity {
+    this.logger.verbose(`🔀 [GET] /client-metadata.json`);
     return this.blueskyService.generateClientMetadata();
   }
 
@@ -17,6 +19,16 @@ export class BlueskyController {
   @Header("Content-Type", "application/json")
   @Header("Access-Control-Allow-Origin", "*")
   getConnectUrl(): ConnectUrlResponseEntity {
+    this.logger.verbose(`🔀 [GET] /url-connect`);
     return this.blueskyService.generateConnectUrl();
+  }
+
+  @Get("callback")
+  @Header("Access-Control-Allow-Origin", "*")
+  getCallback(@Query() query: string) {
+    this.logger.verbose(
+      `🔀 [GET] /callback` + (query ? " " + query.toString() : ""),
+    );
+    throw new Error("Method not implemented.");
   }
 }
