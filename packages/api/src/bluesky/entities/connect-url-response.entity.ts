597386ba7fa38606740679ab54ab763be223989a
Fabien Sanchez <fabien.sanchez@citopia.fr>
2026-02-16 08:30:49 +0100

    fix: simplify ConnectUrlResponseEntity constructor by removing unused parameters

--- packages/api/src/bluesky/entities/connect-url-response.entity.ts
+++ packages/api/src/bluesky/entities/connect-url-response.entity.ts
@@ -1,11 +1,7 @@
 export class ConnectUrlResponseEntity {
   url: string;
-  state?: string;
-  challenge?: string;
 
-  constructor(response: { url: string; state?: string; challenge?: string }) {
+  constructor(response: { url: string }) {
     this.url = response.url;
-    // this.state = response.state;
-    // this.challenge = response.challenge;
   }
 }
